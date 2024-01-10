59ce247024a98e327f59178156bfe0777f8a4155
Belga <belga.1212@gmail.com>
2024-01-10 13:55:43 +0100

    Added transitioning between functions

--- Assets/Scripts/Graph.cs
+++ Assets/Scripts/Graph.cs
@@ -13,14 +13,17 @@ public class Graph : MonoBehaviour
 	[SerializeField]
 	FunctionLibrary.FunctionName function;
 
-	[SerializeField, Min(0f)]
-	float functionDuration = 1f;
-
 	[SerializeField]
 	TransitionMode transitionMode;
 
+	[SerializeField, Min(0f)]
+	float functionDuration = 1f, transitionDuration = 1f;
+
+
 	Transform[] points;
 	float duration;
+	bool transitioning;
+	FunctionLibrary.FunctionName transitionFunction;
 
 	void Awake()
 	{
@@ -40,12 +43,30 @@ public class Graph : MonoBehaviour
 	void Update()
 	{
 		duration += Time.deltaTime;
-		if (duration >= functionDuration)
+		if (transitioning)
+		{
+			if (duration >= transitionDuration)
+			{
+				duration -= transitionDuration;
+				transitioning = false;
+			}
+		}
+		else if (duration >= functionDuration)
 		{
 			duration -= functionDuration;
+			transitioning = true;
+			transitionFunction = function;
 			PickNextFunction();
 		}
-		UpdateFunction();
+
+		if (transitioning)
+		{
+			UpdateFunctionTransition();
+		}
+		else
+		{
+			UpdateFunction();
+		}
 	}
 
 	void PickNextFunction()
@@ -73,6 +94,28 @@ public class Graph : MonoBehaviour
 			}
 			float u = (x + 0.5f) * step - 1f;
 			points[i].localPosition = f(u, v, time);
+		}
+	}
+
+	void UpdateFunctionTransition()
+	{
+		FunctionLibrary.Function from = FunctionLibrary.GetFunction(transitionFunction);
+		FunctionLibrary.Function to = FunctionLibrary.GetFunction(function);
+		float progress = duration / transitionDuration;
+
+		float step = 2f / resolution;
+		float time = Time.time;
+		float v = 0.5f * step - 1f;
+		for (int i = 0, x = 0, z = 0; i < points.Length; i++, x++)
+		{
+			if (x == resolution)
+			{
+				x = 0;
+				z++;
+				v = (z + 0.5f) * step - 1f;
+			}
+			float u = (x + 0.5f) * step - 1f;
+			points[i].localPosition = FunctionLibrary.Morph(u, v, time, from, to, progress);
 		}
 	}
 
